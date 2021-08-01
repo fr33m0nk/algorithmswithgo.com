65e1170d518247b87244efce054014fc45c86927
Fr33M0nk <43627165+fr33m0nk@users.noreply.github.com>
2021-08-01 07:53:09 +0000

    [fr33m0nk]: Solves base to decimal

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -9,5 +9,31 @@ package module01
 //   BaseToDec("1110", 2) => 14
 //
 func BaseToDec(value string, base int) int {
-	return 0
+	charset := map[string]int{
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,                                      
+    "A": 10,
+    "B": 11,
+    "C" : 12,
+    "D" : 13,
+    "E" : 14,
+    "F" : 15,            
+  }
+  res := 0
+  multiplier := 1
+  runes := []rune(value)
+  for i := len(runes) -1; i >= 0; i-- {
+    literal := charset[string(runes[i])]
+    res = res + literal * multiplier
+    multiplier *= base
+  }
+  return res
 }
