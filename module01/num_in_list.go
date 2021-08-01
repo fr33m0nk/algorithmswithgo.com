809b0ef790681f7cd0d3408be329e4dbcf2326b0
Fr33M0nk <43627165+fr33m0nk@users.noreply.github.com>
2021-08-01 05:34:32 +0000

    [fr33m0nk]: Completes first exercisw

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -3,5 +3,10 @@ package module01
 // NumInList will return true if num is in the
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
-	return false
+	for _, s := range list {
+    if s == num {
+      return true
+    }
+  }
+  return false
 }
