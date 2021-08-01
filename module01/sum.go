a35f464c6ca874799aee9aa07b394fb4b7ab1583
Fr33M0nk <43627165+fr33m0nk@users.noreply.github.com>
2021-08-01 05:46:46 +0000

    [fr33m0nk]: Solves second exercise using recursion

--- module01/sum.go
+++ module01/sum.go
@@ -3,9 +3,18 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	var sum int;
-  for _, val := range numbers {
-    sum += val
+	//Using loop
+
+  // var sum int;
+  // for _, val := range numbers {
+  //   sum += val
+  // }
+  // return sum
+
+  //Using recursion
+  if len(numbers) == 0 {
+    return 0
   }
-  return sum
+
+  return numbers[0] + Sum(numbers[1:])
 }
