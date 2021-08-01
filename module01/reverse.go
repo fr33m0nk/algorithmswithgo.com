30492974e75728e4502635a4175d0e1c252b718d
Fr33M0nk <43627165+fr33m0nk@users.noreply.github.com>
2021-08-01 06:21:18 +0000

    [fr33m0nk]: Solves exercise 3 with different methods

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,5 +1,7 @@
 package module01
 
+// import ("strings")
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
@@ -7,5 +9,22 @@ package module01
 //   Reverse("alphabet") => "tebahpla"
 //
 func Reverse(word string) string {
-	return ""
+  // var res string
+  // for _, r := range word {
+  //   res = string(r) + res
+  // }
+	// return res
+
+  // var sb strings.Builder
+  // runes := []rune(word)
+  // for i:= len(runes)-1; i >= 0; i-- {
+  //   sb.WriteRune(runes[i])
+  // }
+  // return sb.String()
+
+  runes := []rune(word)
+  for i, j:= 0, len(runes) -1; i < j; i, j = i+1, j-1 {
+    runes[i], runes[j] = runes[j], runes[i]
+  }
+  return string(runes)
 }
